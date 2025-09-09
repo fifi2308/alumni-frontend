dfeff2cff74180844246c9d31a77096214653e2f
Fatou Fall <fatoufall0320@gmail.com>
2025-09-09 17:31:45 +0000

    Initial commit - projet alumni-frontend

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private API_URL = 'http://localhost:8000/api'; // ton Laravel tourne sur 8000 par défaut
+
+  constructor(private http: HttpClient) { }
+
+  register(user: any): Observable<any> {
+    return this.http.post(`${this.API_URL}/register`, user);
+  }
+
+  login(credentials: any): Observable<any> {
+    return this.http.post(`${this.API_URL}/login`, credentials);
+  }
+
+  getProfile(): Observable<any> {
+    return this.http.get(`${this.API_URL}/profile`);
+  }
+
+  updateProfile(data: any): Observable<any> {
+    return this.http.put(`${this.API_URL}/profile`, data);
+  }
+
+  resetPassword(email: string): Observable<any> {
+    return this.http.post(`${this.API_URL}/password/reset`, { email });
+  }
+}
---
+++ src/app/users/users.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UsersRoutingModule } from './users-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    ResetPasswordComponent
+  ],
+  imports: [
+    CommonModule,
+    UsersRoutingModule
+  ]
+})
+export class UsersModule { }
